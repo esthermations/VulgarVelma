2da63bd4c2c9804d02286cbccaf012f8a52e66b2
Esther O'Keefe <esthermations@gmail.com>
2021-01-15 16:05:46 +1030

    Attempting to get SSL going

--- src/vulgarvelma.adb
+++ src/vulgarvelma.adb
@@ -3,24 +3,37 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 with AWS.Client;
 with AWS.Response;
+with GNATCOLL.JSON;
 
 procedure VulgarVelma is
-   User_Agent : constant String := "VulgarVelma 0.0.0";
 
-   Gateway_Get_Response      : AWS.Response.Data;
-   Gateway_Get_Response_Body : Unbounded_String;
+   --  type Snowflake is new Interfaces.Unsigned_64;
+
+   User_Agent : constant String :=
+      "VulgarVelma (https://github.com/esthermations/VulgarVelma, 0.0.0)";
+
+   Connection : AWS.Client.HTTP_Connection;
+
+   Response      : AWS.Response.Data;
+   Response_Body : Unbounded_String;
 begin
+
+   AWS.Client.Create
+      (Connection  => Connection,
+       Host        => "https://discord.com/api/v8",
+       Certificate => "/Users/estherokeefe/Documents/vulgarvelma.cer",
+       User_Agent  => User_Agent);
+
+   AWS.Client.Get (Connection => Connection,
+                   Result     => Response,
+                   URI        => "/gateway");
+
+   Response_Body := AWS.Response.Message_Body (Response);
+
    --  Send this to request a gateway endpoint which we will connect to.
-   Gateway_Get_Response :=
-      AWS.Client.Get (URL        => "https://discord.com/api/v8/gateway",
-                      User_Agent => User_Agent);
-
-   Gateway_Get_Response_Body :=
-      AWS.Response.Message_Body (Gateway_Get_Response);
-
    Put_Line ("Response: ");
    Put ("[");
-   Put (To_String (Gateway_Get_Response_Body));
+   Put (To_String (Response_Body));
    Put ("]");
    New_Line;
 end VulgarVelma;
